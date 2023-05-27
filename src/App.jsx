74fb5f0c9694bb1a9270351bd17d92e28ff1cc75
jason@alley.co <jcameron@alley.com>
2023-05-27 01:29:27 -0400

    added form submit handler for todos

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,28 @@ import "./styles.css";
 
 export default function App() {
   const [todo, setTodo] = useState("")
+  const [todos, setTodos] = useState([])
 
   const handleTodoChange = (e) => {
     setTodo(e.target.value)
   }
 
+  /* 
+    1. Prevent the default behavior of the form.
+    2. Create a new todo object with the todo and a unique id.
+    3. Add the todo object to the todos array.
+   */
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setTodos(currentTodos => {
+      return [...currentTodos, {todo, id: crypto.randomUUID(),  title: todo, completed: false },
+    ]
+    })
+  }
 
   return(
     <div className="wrapper">
-      <form className="todo-form">
+      <form className="todo-form" onSubmit={handleSubmit}>
         <div className="todo-form__input">
           <label htmlFor="todo">Add Item</label>
           <input value={todo} onChange={handleTodoChange} type="text" id="todo" />
